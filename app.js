41ff4317e3880dff8a67bd86ef0b69c58ec8bc6f
Brian Kenji Aguilar <anjacular@gmail.com>
2019-03-16 14:45:23 -0400

    setting up backend for project

---
+++ app.js
@@ -0,0 +1,37 @@
+// Package Imports
+const createError = require('http-errors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const router = express.Router();
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Allows servers to communicate with each other
+app.use((req, res, next) =>{
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  next();
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  // res.sendStatus(err.status)
+  res.status(err.status || 500);
+  res.send(err.status);
+});
+
+module.exports = app;
