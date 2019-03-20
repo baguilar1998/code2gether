7a396d4cc63dc31fdc6e89b9d8a3d09f89dd1128
Brian Kenji Aguilar <anjacular@gmail.com>
2019-03-20 12:20:19 -0400

    adding setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { SignupComponent } from './landing/signup/signup.component';
+import { SigninComponent } from './landing/signin/signin.component';
+import { ForgotPasswordComponent } from './landing/forgot-password/forgot-password.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './main/navbar/navbar.component';
+import { ProjectEditorComponent } from './main/project-editor/project-editor.component';
+import { FileListComponent } from './main/project-editor/file-list/file-list.component';
+import { CodeEditorComponent } from './main/project-editor/code-editor/code-editor.component';
+import { CompilerComponent } from './main/project-editor/compiler/compiler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SignupComponent,
+    SigninComponent,
+    ForgotPasswordComponent,
+    MainComponent,
+    NavbarComponent,
+    ProjectEditorComponent,
+    FileListComponent,
+    CodeEditorComponent,
+    CompilerComponent
   ],
   imports: [
     BrowserModule
