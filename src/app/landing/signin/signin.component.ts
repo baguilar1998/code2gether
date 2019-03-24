0b83a27ebb52626b6b1cb9a8b2a678eb687980fc
Brian Kenji Aguilar <anjacular@gmail.com>
2019-03-24 11:47:36 -0400

    fixing up logic for landing page and modals

--- src/app/landing/signin/signin.component.ts
+++ src/app/landing/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output  } from '@angular/core';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
+  @Output() stateChange = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeState(state: string): void {
+    this.stateChange.emit(state);
+  }
 }
--- src/app/landing/signup/signup.component.ts
+++ src/app/landing/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  @Output() stateChange = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeState(state: string): void {
+    this.stateChange.emit(state);
+  }
 }
