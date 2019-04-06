8d08d003083d715e3529fc9e30712078ffd6c088
Brian Kenji Aguilar <anjacular@gmail.com>
2019-04-06 11:11:33 -0400

    set up the form for create new project

--- src/app/main/create-new-project/create-new-project.component.ts
+++ src/app/main/create-new-project/create-new-project.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-create-new-project',
   templateUrl: './create-new-project.component.html',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateNewProjectComponent implements OnInit {
 
+  languages: string[];
+  projectName: string;
+  description: string;
+  language: string;
+  form: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.projectName = '';
+    this.description = '';
+    this.language = 'Java';
+    this.languages = ['Java', 'C++', 'Python', 'C', 'Javascript'];
+    this.form = new FormGroup({
+      'projectName': new FormControl(this.projectName, {validators: [Validators.required]}),
+      'description': new FormControl(this.description, {validators: [Validators.required]}),
+      'language': new FormControl(this.language, {validators: [Validators.required]})
+    });
+  }
+
+  createProject(): void {
+    this.projectName = this.form.value.projectName;
+    this.description = this.form.value.description;
+    this.language = this.form.value.language;
+
   }
 
 }
--- src/app/models/Project.ts
+++ src/app/models/Project.ts
@@ -2,7 +2,7 @@ import { User } from './User';
 
 export class Project {
   _id: string;
-  owner: string;
+  owner: User;
   name: string;
   editors: User[];
   maxUsers: number;
